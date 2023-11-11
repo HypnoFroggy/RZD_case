0774b21fe4f4555247a21db647cd3db4e6e16fa7
Mitya <dmitrij.mal.tim.04@gmail.com>
2023-11-11 22:56:30 +0300

    чтение псевдословаря от нейронной сети

--- video/VideoApp/views.py
+++ video/VideoApp/views.py
@@ -1,18 +1,13 @@
 from django.shortcuts import render
-from .forms import UploadFileForm
 from .filehandle import handle_uploaded_file
 from django.http import HttpResponseRedirect
+max = 0
 def index(request):
     if request.method == "POST":
-        #form = UploadFileForm(request.POST, request.FILES)
-        #print(type(request.FILES))
         if request.FILES:
-            print(type(request.FILES.get("f")))
-            handle_uploaded_file(request.FILES.get("f"))
+            global max
+            max = handle_uploaded_file(request.FILES.get("f"))
             return HttpResponseRedirect("/success")
-    #else:
-        #form = UploadFileForm()
     return render(request, "index.html")
-
 def success(request):
-    return render(request, "success.html")
+    return render(request, "success.html",context={"result":max})
